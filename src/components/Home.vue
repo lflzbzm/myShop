fef110d78a07a03d766d4e5e6e82e9c76f11f52d
smile <508842342@qq.com>
2020-04-28 14:29:49 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-container class="home_container">
+        <!--头部-->
+        <el-header>Header
+            <el-button type="info" @click="loginout">退出</el-button>
+        </el-header>
+        <!---->
+        <el-container>
+            <!--左侧菜单-->
+            <el-aside width="200px">Aside</el-aside>
+            <!--右侧主体-->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    loginout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home_container{
+    height: 100%;
+}
+.el-header{
+    background-color: #373d41;
+}
+.el-aside{
+    background-color: #333744;
+}
+.el-main{
+    background-color: #eaedf1;
+}
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import { Button, Container, Header, Aside, Main, Form, FormItem, Input, Message } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
